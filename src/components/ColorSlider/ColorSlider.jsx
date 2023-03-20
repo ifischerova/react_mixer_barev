759ef38021ac5732e2f0754a361246f52b624e13
Ivuska <fischerova.ivka@gmail.com>
2023-03-20 15:23:11 +0100

    Set value as state prop to move the slider.

--- src/components/ColorSlider/ColorSlider.jsx
+++ src/components/ColorSlider/ColorSlider.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './color_slider.css';
 
 const ColorSlider = ({ baseColor, colorName, onValueChange }) => {
 
     const handleValueChange = (e) => {
         onValueChange(e.target.value);
+        setValue(e.target.value)
     }
+
+    const [value, setValue] = useState(0)
 
     return (
         <>
-            <label htmlFor="red">{colorName}</label>
+            <label htmlFor={`${baseColor}`}>{colorName}</label>
 			<input
                 type="range"
                 className={`slider slider--${baseColor}`}
-                id="redSlider"
+                id={`${baseColor}Slider`}
                 min="0"
                 max="255"
-                value="0"
+                value={value}
                 onChange={handleValueChange}
              />
         </>
